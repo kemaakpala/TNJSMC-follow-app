83dfd35e10c772db8b294c0bc72ca7fbbee002d4
Patrick Akpala <patrick.akpala@lloydsbanking.com>
2022-02-26 00:50:38 +0000

    Building a RESTful API: Parsing Payloads: using streams and event emitters

--- index.js
+++ index.js
@@ -4,16 +4,17 @@
 
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
-var server = http.createServer(function(req,res){
+var server = http.createServer(function (req, res) {
   // 1. Get the url and parse it
   var parsedUrl = url.parse(req.url, true);
 
   // 2. Get the path from the url
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'')
-  
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
   // 3. Get the query string as an object
   var queryStringObject = parsedUrl.query;
 
@@ -23,15 +24,26 @@ var server = http.createServer(function(req,res){
   // 5. Get the Headers as an object
   var headers = req.headers;
 
-  // 6. Send the response
-  res.end("Hello world\n")
+  // 6. Get the payload, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  // end event will always get called with or without a payload
+  req.on('end', function () {
+    buffer += decoder.end();
 
-  // 7. Log the request path
-  console.log('Request recieved with these headers', headers);
+    // 7. Send the response
+    res.end("Hello world\n")
+
+    // 8. Log the request path
+    console.log('Request recieved with these payload: ', buffer);
+  })
 
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, function() {
+server.listen(3000, function () {
   console.log("The server is listening on port 3000 now");
 });
