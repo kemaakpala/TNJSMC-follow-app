33c35d1a55195d106371d5129ccffe7039ac40a3
Patrick Akpala <patrick.akpala@lloydsbanking.com>
2022-02-26 13:27:37 +0000

    Building a RestfulApi: Adding HTTPS Support
    added https support and generated ssl certs
    added functionality to serve both http and https.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TNJSMC-follow-app
+
 The Node.js Master Class Follow App
+
+# How to Generate ssl certificate
+
+Create a https folder and cd into it
+```
+mkdir https
+cd https
+```
+
+enter in your terminal the below command to generate a key and cert pem using openssl
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
+```
+Generating a 2048 bit RSA private key
+..................+++
+.....................................................+++
+writing new private key to 'key.pem'
+-----
+You are about to be asked to enter information that will be incorporated
+into your certificate request.
+What you are about to enter is what is called a Distinguished Name or a DN.
+There are quite a few fields but you can leave some blank
+For some fields there will be a default value,
+If you enter '.', the field will be left blank.
+-----
+Country Name (2 letter code) []:<enter country name>
+State or Province Name (full name) []:<enter state>
+Locality Name (eg, city) []:<enter city name>
+Organization Name (eg, company) []:<enter company name>
+Organizational Unit Name (eg, section) []:<enter org unit name>
+Common Name (eg, fully qualified host name) []:<enter common name>
+Email Address []:<enter dummy email address>
+```
--- index.js
+++ index.js
@@ -3,12 +3,41 @@
 // Dependencies
 
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function (req, res) {
+// Instantiating the HTTP server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res)
+});
+
+// Start the server
+httpServer.listen(config.httpPort, function () {
+  console.log("The server is listening on port " + config.httpPort);
+});
+
+
+// Instantiating the HTTPS server
+var httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res)
+});
+
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+  console.log("The server is listening on port " + config.httpsPort);
+});
+
+
+// All the server logic for both the http and https server.
+var unifiedServer = function (req, res) {
   // 1. Get the url and parse it
   var parsedUrl = url.parse(req.url, true); // setting the second parameter to true allows querystring to be returned as an object instead of an undecoded string.
 
@@ -77,12 +106,8 @@ var server = http.createServer(function (req, res) {
 
   })
 
-});
+};
 
-// Start the server
-server.listen(config.port, function () {
-  console.log("The server is listening on port "+config.port+ " in "+config.envName+" mode");
-});
 
 // Define the handlers
 var handlers = {}
