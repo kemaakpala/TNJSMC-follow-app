6bad8e4b3e2ff2a0683081ee9a1a45eb479d9dd4
Patrick Akpala <patrick.akpala@lloydsbanking.com>
2022-02-26 00:20:08 +0000

    Building Restful API: Parsing Request Paths.

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 // Dependencies
 
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req,res){
+  // 1. Get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // 2. Get the path from the url
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+  // 3. Send the response
   res.end("Hello world\n")
+
+  // 4. Log the request path
+  console.log('Request received on path: '+ trimmedPath);
+
 });
 
 // Start the server, and have it listen on port 3000
